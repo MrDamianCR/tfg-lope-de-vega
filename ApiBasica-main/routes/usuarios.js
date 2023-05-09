b662efeac29047b8ed1ffc9e651a7ed9269cfe33
Alvaro <alvarolosadaromero@gmail.com>
2023-05-09 13:11:03 +0200

    Update usuarios.js

--- ApiBasica-main/routes/usuarios.js
+++ ApiBasica-main/routes/usuarios.js
@@ -40,6 +40,49 @@ router.get("/usuarios/:id_usuario", (req, res) => {
     });
 });
 
+
+/*const crypto = require('crypto');
+const algorithm = 'aes-256-ctr';
+const key = 'clave-secreta';
+
+const encrypt = (text) => {
+    const iv = crypto.randomBytes(16);
+    const cipher = crypto.createCipheriv(algorithm, key, iv);
+    const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
+    return `${iv.toString('hex')}:${encrypted.toString('hex')}`;
+};
+
+const decrypt = (text) => {
+    const [ivHex, encryptedHex] = text.split(':');
+    const iv = Buffer.from(ivHex, 'hex');
+    const encrypted = Buffer.from(encryptedHex, 'hex');
+    const decipher = crypto.createDecipheriv(algorithm, key, iv);
+    const decrypted = Buffer.concat([decipher.update(encrypted), decipher.final()]);
+    return decrypted.toString();
+};
+
+router.get("/usuarios/:id_usuario", (req, res) => {
+    const userId = req.params.id_usuario;
+    const sql =
+        encrypt("SELECT nombre_usuario, apellidos_usuario, email_usuario, pass_usuario, fecha_nacimiento_usuario, telefono_usuario, email_recuperacion_usuario FROM usuarios WHERE id_usuario = ?");
+
+    connection.query(decrypt(sql), [userId], (error, results) => {
+        if (error) {
+            console.error(decrypt("Error al obtener usuario: "), error);
+            res.status(500).send(decrypt("Error al obtener usuario"));
+            return;
+        }
+
+        if (results.length === 0) {
+            res.status(404).send(decrypt("Usuario no encontrado"));
+            return;
+        }
+
+        res.json(results[0]);
+    });
+});*/
+
+
 // Añadir usuario
 router.post("/usuarios", (req, res) => {
     const {
