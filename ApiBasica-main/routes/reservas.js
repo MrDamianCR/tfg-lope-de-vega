ad3f4b11dbe1702871ef492df4cf9cc0ff12b4bd
Alvaro <alvarolosadaromero@gmail.com>
2023-06-07 12:26:23 +0200

    Update reservas.js

--- ApiBasica-main/routes/reservas.js
+++ ApiBasica-main/routes/reservas.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const connection = require("../db");
 
-//Mostrar Reservas 
+//Mostrar 
 router.get("/reservas", (req, res) => {
 
 
